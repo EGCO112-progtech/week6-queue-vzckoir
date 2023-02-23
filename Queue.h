1f1188b36ab8ba39dffd21de5104564f441ed5af
Mingmanas Sivaraksa <62439174+AjMing@users.noreply.github.com>
2023-02-23 15:53:33 +0000

    Initial commit

---
+++ Queue.h
@@ -0,0 +1,35 @@
+
+
+typedef struct {
+	 NodePtr headPtr,tailPtr;
+	int size;
+}Queue;
+
+
+void enqueue_struct(Queue* q, int x){
+  Node *new_node=(Node*) malloc(sizeof(Node));
+if(new_node){ 
+   	new_node->data=x;
+		new_node->nextPtr=NULL;
+     if(q->headPtr==NULL) q->headPtr=new_node;
+      	else q->tailPtr->nextPtr=new_node ;             
+	   q->tailPtr=new_node;
+     q->size++;
+ }
+}
+
+
+int dequeue_struct(Queue *q){
+   NodePtr t=q->headPtr;
+   if(t){
+   int value= t->data;
+   q->headPtr= t->nextPtr; // NULL
+   if(q->headPtr==NULL) q->tailPtr=NULL;
+   free(t);
+   q->size--;
+   return value;
+   }
+   printf("Empty queue");
+   return 0;
+}
+
---
+++ main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "Node.h"
+
+#include "Queue.h"
+
+int main(int argc , char **argv) {
+ /*  NodePtr headPtr=NULL;
+   NodePtr tailPtr=NULL;*/
+   Queue  q;
+   q. headPtr=NULL;
+   q.tailPtr=NULL;
+   q.size=0;
+   
+   int i,x;
+   
+
+ for(i=1;i<argc;i++){
+        if(strcmp(argv[i],"x")==0){
+            x=dequeue_struct(&q); 
+            printf("dequeing %d\n",x);
+        }
+        else {
+       enqueue_struct(&q, atoi(argv[i]));
+           
+        }
+ }
+  return 0;
+}
