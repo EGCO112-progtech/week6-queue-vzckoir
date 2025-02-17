1a3f7e195e6274e38a9f44602c14dc4ccc106312
vzckoir <zabit.mad@student.mahidol.edu>
2025-02-17 06:52:57 +0000

    a

--- Queue.h
+++ Queue.h
@@ -1,46 +1,38 @@
-#define Queue_H
-#include "Node.h"
-
 typedef struct {
-	 NodePtr headPtr,tailPtr;
-	int size;
+  NodePtr headPtr,tailPtr;
+ int size;
 }Queue;
 
 
 void enqueue_struct(Queue* q, int x){
-  Node *new_node=(Node*) malloc(sizeof(Node));
-if(new_node){
-  new_node->data=x;
-  new_node->nextPtr=NULL;
- }
- if(q->tailPtr){
-  q->tailPtr->nextPtr=new_node;
- }
- else {
-  q->headPtr = new_node;
- }
+ Node *new_node=(Node*) malloc(sizeof(Node));
+ if(new_node){ 
+ new_node->data=x;
+ new_node->nextPtr=NULL;
+}
+if(q->size==0) q->headPtr=new_node;
+ else q->tailPtr=new_node;
  q->tailPtr=new_node;
  q->size++;
+
 }
+
 
 
 int dequeue_struct(Queue *q){
+
   if(q->size>0){
     NodePtr t=q->headPtr;
-    int value=t->data;
-    if(q->size==0)
-      q->headPtr=NULL;
-      q->size--;
-      free(t);
-      return value;
+  if(q->size==0)
+   q->headPtr= q->headPtr->nextPtr;
+   q->size--;
+  } 
+ Node* t = q->headPtr;
+  if(t){
+  int value= t->data;
+ free(t);
+  return value;
   }
+  printf("Empty queue");
+  return 0;
 }
-   /*if(t){
-   int value= t->data;
-       //Finish dequeue 
-   return value;
-   }
-   printf("Empty queue");
-   return 0;
-}*/
-
